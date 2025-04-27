27b36be17d11d5f9f3a08ab80bce7f757e8bc6e3
LucianoPastocchi <lucianodpastocchi@gmail.com>
2025-04-27 20:33:13 -0300

    first commit

---
+++ src/main/java/com/sampa/SampaApplication.java
@@ -0,0 +1,13 @@
+package com.sampa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampaApplication.class, args);
+	}
+
+}
